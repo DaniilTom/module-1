ce100bd18efc7ce17fa1f9e5d4e65b0ff0cb7dc6
Name <you@example.com>
2022-11-13 21:11:03 +0300

    Fix typos

--- src/com/example/module1/main.java
+++ src/com/example/module1/main.java
@@ -1,11 +1,11 @@
-package com.example.module1
+package com.example.module1;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("ok");
    }
   
-  public void print() {
-    System.out.ptinrln("from module 1"); 
+  public static void print() {
+    System.out.println("from module 1"); 
   }
 }
